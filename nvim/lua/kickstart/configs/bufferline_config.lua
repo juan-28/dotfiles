126c96bff2a3d78ae359b601dd18e46bd77f6638
Surya Pranav Sukumaran <ps2pranav@gmail.com>
2024-05-25 04:00:24 -0400

    bufferline dashboard fix

--- nvim/lua/kickstart/configs/bufferline_config.lua
+++ nvim/lua/kickstart/configs/bufferline_config.lua
@@ -16,7 +16,7 @@ bufferline.setup {
     show_buffer_close_icons = true,
     show_close_icon = true,
     enforce_regular_tabs = false,
-    always_show_bufferline = true,
+    always_show_bufferline = false,
     offsets = { { filetype = 'NvimTree', text = 'File Explorer', highlight = 'Directory', separator = true } },
     color_icons = true,
     highlights = {
