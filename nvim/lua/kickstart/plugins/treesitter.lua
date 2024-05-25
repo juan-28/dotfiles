38240c2e55d712710892094ea6eae9d1343f2b1e
Surya Pranav Sukumaran <ps2pranav@gmail.com>
2024-05-25 02:31:16 -0400

    treesitter node incremental keymap

--- nvim/lua/kickstart/plugins/treesitter.lua
+++ nvim/lua/kickstart/plugins/treesitter.lua
@@ -4,6 +4,17 @@ return {
     build = ':TSUpdate',
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      incremental_selection = {
+
+        enable = true,
+        keymaps = {
+
+          init_selection = '<C-space>',
+          node_incremental = '<C-space>',
+          scope_incremental = false,
+          node_decremental = '<bs>',
+        },
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
